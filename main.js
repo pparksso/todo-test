5e23aceb6ecf592c5198dcb106c3cbec85ea3b9a
pparksso <thgml5143@hanmail.net>
2024-02-05 13:15:00 +0900

    feat: 기능 완료

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ const list = document.getElementById('list');
 const storageData = localStorage.getItem('todo');
 let todoArr = storageData ? JSON.parse(storageData) : [];
 
-window.addEventListener('load', getList())
+window.addEventListener('load', getList());
 
-// 로컬스토리지에 저장하기,
+// 배열에서 제일 큰 아이디를 가진 객체의 아이디 + 1 을 아이디로 지정
+// 로컬스토리지에 저장하기
 function saveInput() {
+    let id;
+    if(todoArr.length > 0) {
+        const list = [...todoArr];
+        const maxItem = list.reduce((acc, cur) => {
+            return cur.id > acc.id ? cur : acc;
+        })
+        id = maxItem.id;
+    } 
     const addTodo = [...todoArr, {
-        id: todoArr.length + 1,
+        id: id ? id + 1 : 1,
         text: input.value,
         check: false
     }]
@@ -21,10 +30,9 @@ function saveInput() {
 
 // 저장 후 불러들이는 함수 만들기
 function getList() {
-    const todoList = JSON.parse(localStorage.getItem('todo'));
-    if(todoList) {
+    if(todoArr) {
         list.innerHTML = ``;
-        todoList.forEach((item, idx)=> {
+        todoArr.forEach((item, idx)=> {
            const li = document.createElement('li');
            const label = document.createElement('label');
            const checkBox = document.createElement('input');
@@ -35,6 +43,8 @@ function getList() {
            checkBox.type = 'checkbox';
            checkBox.id = 'c' + item.id;
            checkBox.dataset.id = item.id;
+           checkBox.dataset.idx = idx;
+           checkBox.checked = item.check;
            checkBox.className = 'checkBox';
            span.innerHTML = item.text;
            delBtn.dataset.id = item.id;
@@ -44,7 +54,8 @@ function getList() {
            label.appendChild(checkBox);
            label.appendChild(span);
            li.appendChild(delBtn);
-           checkBox.addEventListener('click', (e) => {changeChechVal(e)});
+           checkBox.addEventListener('click', (e) => {changeCheckVal(e)});
+           delBtn.addEventListener('click',(e) => {removeItemHandler(e)});
         });
         input.value = '';
     }
@@ -55,14 +66,30 @@ function addItem() {
     getList();
 }
 
+// 변경사항 저장
+function changeCheckVal(e) {
+    const idx = e.target.dataset.idx;
+    const checkVal = e.target.checked;
+    const copyList = [...todoArr];
+    copyList[idx].check = checkVal;
+    localStorage.setItem('todo',JSON.stringify(copyList));
+    todoArr = copyList;
+    getList();
+}
+
+// 삭제
+function removeItemHandler(e) {
+    const id = parseInt(e.target.dataset.id);
+    const newList = [...todoArr].filter((item) => {
+        return item.id !== id;
+    })
+    localStorage.setItem('todo',JSON.stringify(newList));
+    todoArr = newList;
+    getList();
+};
+
 form.addEventListener('submit',(event) => {
     event.preventDefault();
     addItem();
-})
+});
 
-// 체크
-// 클릭 한 노드의 data값을 추출하여 로컬스토리지에 저장..
-
-
-// 삭제
-// 클릭 한 노드의 data값을 추출하여 해당 아이템 삭제 후 로컬스토리지에 저장, 호출
